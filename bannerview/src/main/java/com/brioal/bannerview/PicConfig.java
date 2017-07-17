ddd2a8cc3f9dbfe1148151bd9ed383bf2768e0dc
Brioal <brioal@foxmail.com>
2017-07-17 22:46:56 +0800

    添加加载失败图片配置

---
+++ bannerview/src/main/java/com/brioal/bannerview/PicConfig.java
@@ -0,0 +1,11 @@
+package com.brioal.bannerview;
+
+/**
+ * email:brioal@foxmail.com
+ * github:https://github.com/Brioal
+ * Created by Brioal on 2017/7/17.
+ */
+
+public class PicConfig {
+    public static int FAILED_PIC = R.mipmap.ic_failed_default;
+}
